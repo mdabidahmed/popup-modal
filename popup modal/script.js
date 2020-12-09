85e63dffa1c6b528d063b54cb78bafaf1522ac33
mernstackdeveloper <49354823+mernstackdeveloper@users.noreply.github.com>
2020-12-09 21:52:00 +0530

    Add files via upload

---
+++ popup modal/script.js
@@ -0,0 +1,7 @@
+function check() {
+  document.querySelector(".bg-modal").style.display = "flex";
+}
+
+document.querySelector(".close").addEventListener("click", function () {
+  document.querySelector(".bg-modal").style.display = "none";
+});
